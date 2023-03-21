21e5633651002106d6974a82123749a974ff6e8b
HelloWorldYessir420 <hablizulvana@365.telkomuniversity.ac.id>
2023-03-21 16:28:27 +0700

    implementasi generic method

--- mod5asli_1302210110/mod5asli_1302210110/Program.cs
+++ mod5asli_1302210110/mod5asli_1302210110/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Runtime.CompilerServices;
+
 Console.WriteLine("Hello, World!");
+
+
+class Penjumlahan
+{
+    static void  PenjumlahanTigaAngka<T>(T a, T b, T c){
+        dynamic q = a;
+        dynamic r = b;
+        dynamic g = c;
+        dynamic jumlah = q + r + g;
+    }
+    public void Main(string[] args)
+    {
+        
+        double q = Console.Read();
+        float r = Console.Read();
+        long g = Console.Read();
+        Console.WriteLine("Masukan angka double " + q);
+        Console.WriteLine("Masukan angka float" + r);
+        Console.WriteLine("Masukan angka long" + g);
+        PenjumlahanTigaAngka(q, r, g);
+
+    }
+}
+
+
