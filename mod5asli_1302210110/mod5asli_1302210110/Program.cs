75115c8184f9b1f00efc3cdabc651bfbd4e2887e
HelloWorldYessir420 <hablizulvana@365.telkomuniversity.ac.id>
2023-03-21 17:23:55 +0700

    final

--- mod5asli_1302210110/mod5asli_1302210110/Program.cs
+++ mod5asli_1302210110/mod5asli_1302210110/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Penjumlahan
+{
+         public static T PenjumlahanTigaAngka<T>(T a, T b, T c)
+        {
+            dynamic q = a;
+            dynamic r = b;
+            dynamic g = c;
+            dynamic jumlah = q + r + g;
+            return jumlah;
+        }
+        
+    
+}
+
+public class database<T>
+{
+    private List<T> StoredData;
+    private List<DateTime> inputDates;
+
+    public void SimpleDataBase()
+    {
+        this.StoredData = new List<T>();
+        this.inputDates = new List<DateTime>();
+    }
+
+    public void AddnewData(T data)
+    {
+        StoredData.Add(data);
+        inputDates.Add(DateTime.Now);
+
+    }
+
+    public void PrintAllData()
+    {
+        for (int i = 0; i < this.inputDates.Count; i++)
+        {
+            Console.WriteLine("Data " + i + "berisi:" + StoredData + ", yang disimpan pada waktu UTC: " + inputDates);
+        }
+    }
+
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        
+        
+        
+        Console.WriteLine(Penjumlahan.PenjumlahanTigaAngka<int>(13, 02, 21));
+
+    }
+}
